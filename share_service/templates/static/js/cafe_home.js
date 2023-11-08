baf0bda07482308cd3a5f95815c0b291bb3d58c4
Ivan Brusov <149581904+IvanBrusovPy@users.noreply.github.com>
2023-11-08 14:49:37 +0200

    foodsharing

---
+++ share_service/templates/static/js/cafe_home.js
@@ -0,0 +1,10 @@
+function toggleBlur(){
+    $('#content').toggleClass('blur');
+    $('#popup').toggleClass('active');
+}
+
+function packOrder(){
+    $('#pack_order').submit();
+}
+
+
---
+++ share_service/templates/static/js/user_home.js
@@ -0,0 +1,11 @@
+$('#filter_city').change(function(){
+    $('#filter_form').submit();
+})
+
+function makeOrder(){
+    $('#create_order').submit();
+}
+
+function pickOrder(){
+    $('#pick_order').submit();
+}
